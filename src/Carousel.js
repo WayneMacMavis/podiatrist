b3d992d27f47e1764d56a43036cbab6d7939d86b
Wayne <w.macmavis83@gmail.com>
2022-02-13 14:39:42 +0200

    mobile responsive

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel from 'react-bootstrap/Carousel';
 import Row from 'react-bootstrap/Row';
